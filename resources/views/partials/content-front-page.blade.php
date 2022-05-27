620ba8ad632a6e60003550451040ca392d0d7fc6
lars-av <1086166@ucn.dk>
2022-05-28 01:39:40 +0200

    g

--- resources/views/partials/content-front-page.blade.php
+++ resources/views/partials/content-front-page.blade.php
@@ -1,4 +1,194 @@
-<section class="flexContainerFp">
+<section>
+    <div class="flex-frontpage">
+    <div class="flex-container-frontpage flex-center text-container-frontpage">
+        <h1>Bo-og Behandlingscenter Blokhus</h1>
+        {!! get_field('frontpage_paragraph') !!}
+        <a class="btnRounded" href="">See available spots</a>
+    </div>
+
+    <div class="flex-container-frontpage image-frontpage">
+        <img src="http://bobehandling.test/wp-content/uploads/2022/05/Purkaervej-scaled.jpg" alt="">
+    </div>
+</div>
+</section>
+
+<section class="wrapper-frontpage">
+<div class="flex-container-entry">
+<div class="focused-entry-points">
+    @php $card = get_field('entrypoint_card') @endphp
+    @foreach ($card as $cards)
+        <a href="{{ $cards['entrypoint_link'] }}" class="card">
+            <img src="{{ $cards['entrypoint_image']['sizes']['large'] }}"
+                @if ($cards['entrypoint_image']['alt']) alt="{{ $cards['entrypoint_image']['alt'] }}" @endif>
+            <p>{{ $cards['entrypoint_text'] }}</p>
+        </a>
+    @endforeach
+</div>
+    <div class="about-fond-container">
+       <h2>{{ get_field('about_fond_title')}} </h2>
+       {!! get_field('about_fond_text') !!}
+    </div>
+</div>
+</section>
+
+<section class="news wrapper-frontpage">
+    <h2>News</h2>
+    <div class="banner">
+        <p>We have new job listings posted</p>     
+        <a href="">Read more</a>
+    </div>
+
+    <div class="news-card-container">
+        <div class="news-card">
+            <img src="http://bobehandling.test/wp-content/uploads/2022/05/Purkaervej-scaled.jpg" alt="">
+            <div class="news-card-text">
+                <h4>Title</h4>
+                <p>Short recap shortrecap</p>
+            </div>
+        </div>
+
+        <div class="news-card">
+            <img src="http://bobehandling.test/wp-content/uploads/2022/05/Purkaervej-scaled.jpg" alt="">
+            <div class="news-card-text">
+                <h4>Title</h4>
+                <p>Short recap shortrecap</p>
+            </div>
+        </div>
+        <div class="news-card">
+            <img src="http://bobehandling.test/wp-content/uploads/2022/05/Purkaervej-scaled.jpg" alt="">
+            <div class="news-card-text">
+                <h4>Title</h4>
+                <p>Short recap shortrecap</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="blue-section">
+
+    <div class="target-group wrapper-frontpage">
+    <!-- Target group text -->
+    <div class="target-group-description flex-center">
+    <h2>Our target group HC</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit eos repudiandae a accusantium ullam aperiam assumenda? Dignissimos laudantium quaerat dolor! Fugiat unde architecto quia tenetur sapiente molestiae delectus debitis modi.</p>
+    </div>
+    @php $i = 1 @endphp
+    @php $x = 1 @endphp
+    @php $targetgroup = get_field('target_group_list') @endphp
+    
+    <!-- Tabs -->
+    <div class="target-group-tabs">
+        <div class="button-container">
+    @foreach ($targetgroup as $tg)
+    <button class="tab active{{$i}} tab-button" data-toggle-target=".tab-content-{{$i}}"> {{ $tg['target_group_list_title'] }}</button>
+    @php $i++ @endphp
+    @endforeach
+        </div>
+    <!-- Content -->
+    @foreach ($targetgroup as $bg)
+    <div class='tab-content tab-content-{{$x}} tab-information active{{$x}}'>
+        {!!$bg['list_of_items'] !!}
+    </div>
+    @php $x++ @endphp
+    @endforeach
+</div>
+</div>
+</section>
+
+<section class="blue-section">
+    <div class="wrapper-frontpage core-values">
+        <div class="core-values-description flex-center">
+        <h2>Core values</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad nihil cum alias architecto, ipsum earum delectus consectetur beatae harum vel similique! Facere aliquam cupiditate minima deleniti voluptas. Assumenda, delectus vel!</p>
+        <a class="btnRounded" href="">Mission, Vision and Values</a>
+        </div>
+        
+        <div class="core-value-grid">
+            <div class="core-value-child flex-center">
+                <h3>Honesty</h3>
+                <p>The foundation for and respecting each other</p>
+            </div>
+            <div class="core-value-child">
+                <h3>Care</h3>
+                <p>The individual situation and well bring uhh</p>
+            </div>
+            <div class="core-value-child">
+                <h3>Care</h3>
+                <p>The individual situation and well bring uhh</p>
+            </div>
+            <div class="core-value-child">
+                <h3>Care</h3>
+                <p>The individual situation and well bring uhh</p>
+            </div>
+            <div class="core-value-child">
+                <h3>Care</h3>
+                <p>The individual situation and well bring uhh</p>
+            </div>
+            <div class="core-value-child">
+                <h3>Care</h3>
+                <p>The individual situation and well bring uhh</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+        <section class="wrapper-frontpage">
+            <h2>Our houses</h2>
+            <div class="slideshow-container">
+                <div class="swiper slideShow">
+                  <div class="swiper-wrapper">
+                    <div class="swiper-slide" data-fancybox="gallery">
+                        <div class="slider-house">
+                            <div class="slider-text-container">
+
+                                <div class="slider-container">
+                                <h3>Purkærhuset</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum velit, aut, non amet dolores modi unde ea voluptatibus minima atque aliquid praesentium adipisci esse. Itaque esse porro tempore iste pariatur.</p>
+                                </div>
+
+                                <a class="btnRounded" href="">See house</a>
+                            </div>
+                            <div class="slider-image-container">
+                                <img src="http://bobehandling.test/wp-content/uploads/2022/05/Purkaervej-scaled.jpg" alt="">
+                            </div>
+                        
+                        </div>
+                        </div>
+                        <div class="swiper-slide" data-fancybox="gallery">
+                            <div class="slider-house">
+                                <div class="slider-text-container">
+                                 <div class="slider-container">
+                                <h3>Purkærhuset</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum velit, aut, non amet dolores modi unde ea voluptatibus minima atque aliquid praesentium adipisci esse. Itaque esse porro tempore iste pariatur.</p>
+                                </div>
+                                    <a class="btnRounded" href="">See house</a>
+                                </div>
+                                <div class="slider-image-container">
+                                    <img src="http://bobehandling.test/wp-content/uploads/2022/05/Purkaervej-scaled.jpg" alt="">
+                                </div>
+                            
+                            </div>
+                            </div>
+                  </div>
+                </div>
+            
+                <div class="swiper-pagination"></div>
+                  </div>
+            </section>
+
+<section class="target-group competencies wrapper-frontpage">
+
+    <div class="competencies-container flex-center">
+        <h2>Competencies and Staff</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem dolorem illo ullam, magni libero saepe odit! Cum sint, rem, veniam nihil deserunt doloribus praesentium error enim minus, quas deleniti.</p>
+    </div>
+    <img src="http://bobehandling.test/wp-content/uploads/2022/05/Purkaervej-scaled.jpg" alt="">
+ 
+
+</section>
+
+
+{{-- <section class="flexContainerFp">
     <div class="flexChildFp">
         <h1>Bo-og Behandlingscenter Blokhus</h1>
         {!! get_field('frontpage_paragraph') !!}
@@ -10,7 +200,7 @@
     </div>
 </section>
 
-<section class="flex-container-entry wrapper-small">
+<section class="flex-container-entry wrapper-small-single-column">
 
     @php $card = get_field('entrypoint_card') @endphp
     @foreach ($card as $cards)
@@ -48,38 +238,10 @@ BIG Flexible content LORE DONT ERASE ME UNTIL U MAKE FLEXIBLE CONTENT
 </section>
 @endif
 @endforeach
- --}}
 
-<section class="target-group wrapper-small flex">
 
-<!-- Target group text -->
-<div class="flex-divider-description">
-<h2>Our target group HC</h2>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit eos repudiandae a accusantium ullam aperiam assumenda? Dignissimos laudantium quaerat dolor! Fugiat unde architecto quia tenetur sapiente molestiae delectus debitis modi.</p>
-</div>
-@php $i = 1 @endphp
-@php $x = 1 @endphp
-@php $targetgroup = get_field('target_group_list') @endphp
 
-<!-- Tabs -->
-<div class="flex-divider-tabs">
-    <div class="button-container">
-@foreach ($targetgroup as $tg)
-<button class="tab active{{$i}} tab-button" data-toggle-target=".tab-content-{{$i}}"> {{ $tg['target_group_list_title'] }}</button>
-@php $i++ @endphp
-@endforeach
-    </div>
-<!-- Content -->
-@foreach ($targetgroup as $bg)
-<div class='tab-content tab-content-{{$x}} tab-information active{{$x}}'>
-    {!!$bg['list_of_items'] !!}
-</div>
-@php $x++ @endphp
-@endforeach
-</div>
-</section>
-
-<section class="wrapper-small">
+<section class="wrapper-small-single-column">
 
 <div class="flex">
 
@@ -117,7 +279,7 @@ BIG Flexible content LORE DONT ERASE ME UNTIL U MAKE FLEXIBLE CONTENT
 </div>
 </section>
 
-<section class="wrapper-large">
+<section class="wrapper-large-single-column">
 <div class="slideshow-container">
     <div class="swiper slideShow">
       <div class="swiper-wrapper">
@@ -148,4 +310,4 @@ BIG Flexible content LORE DONT ERASE ME UNTIL U MAKE FLEXIBLE CONTENT
 
     <div class="swiper-pagination"></div>
       </div>
-</section>
+</section> --}}
