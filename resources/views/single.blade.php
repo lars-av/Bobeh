bbbf018ad0b9a3edd23ee8e22d45f39bdd74e602
FrederikSL <74176662+FrederikSL@users.noreply.github.com>
2022-05-30 10:06:04 +0200

    1
    1

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
-
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-single-'.get_post_type())
-    I am a post - Check single.blade for my markup
-    Forture lars - make IF statement that checks the post's type and changes markup depending wether it is house or news
-  @endwhile
+<input type="checkbox" id="checkBox" name="ticked" onClick= >
+
+{{-- Side navigation --}}
+  <div class="wrapper-large">
+    @if ("true" == "true")
+    <div class="wrap-side-nav">Div1</div>
+    @endif
+
+    {{-- Content --}}
+    <div  @if("true" == "true") class="wrap-side-content" @else class="wrap-only-content" @endif>
+      @include('partials/houses.house-intro')
+      @include('partials/houses.about-house')
+      @include('partials/houses.activities')
+      @include('partials/houses.responsibility-section')
+      @include('partials/houses.living-conditions')
+    </div>
+  </div>
 @endsection
+
+<script>
+
+
+</script>
