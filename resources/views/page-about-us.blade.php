cf9cb16010e3b3b77f18069064f2976024cc6a5d
Baranuel <samuel.baran98@gmail.com>
2022-05-30 17:44:45 +0200

    slider not working

---
+++ resources/views/page-about-us.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+<input type="checkbox" id="checkBox" name="ticked" onClick= >
+
+{{-- Side navigation --}}
+  <div class="wrapper-large">
+    @if ("true" == "true")
+    <div class="wrap-side-nav">
+      <div class="side-nav">
+        <ul>
+           <a href="#social-life"><li class="anchor" >Social Life</li></a>
+           <a href="#day-to-day"><li class="anchor" >Day to Day Life</li></a>
+           <a href="#process-of-applying"><li class="anchor" >Process of applying</li></a>
+           <a href="#self-determination"><li class="anchor" >Self Determiation</li></a>
+           <a href="#duty-of-care"><li class="anchor" >Duty of care</li></a>
+           <a href="#involvement-in-development"><li class="anchor" >Involvement In Development</li></a>
+        </ul>
+      </div>
+    </div>
+    @endif
+
+    {{-- Content --}}
+    <div  @if("true" == "true") class="wrap-side-content" @else class="wrap-only-content" @endif>
+      @include('partials/about-us.hero-text')
+      @include('partials/about-us.core-values')
+      @include('partials/about-us.core-goals')
+    </div>
+  </div>
+@endsection
---
+++ resources/views/partials/about-us/core-goals.blade.php
@@ -0,0 +1,102 @@
+<section id="core-goals">
+<h2>Core Goals</h2>
+
+<ul class="slider">
+  <div class="window">
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+  <li class="goal-specific">
+    <div class="goal">
+      <h3 class="goal-title">Safe home</h3>
+      <p class="goal-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum natus aperiam saepe blanditiis itaque repellendus vel illum deserunt molestiae!</p>
+    </div>
+    <div class="testimony">
+      <h4 class="testimony-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quos!</h4>
+      <p class="resident-author">Residnet testimony</p>
+    </div>
+  </li>
+</div>
+</ul>
+<button data-button >change</button>
+</section>
+
+<script>
+  let title = document.querySelector('.goal-title')
+  const goals = document.querySelectorAll('.goal-specific');
+
+  const button = document.querySelector('[data-button]')
+
+
+
+
+
+console.log(goals)
+</script>
