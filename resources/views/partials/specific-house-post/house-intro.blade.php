2f9adfe891ab33024be67e58d44528c09b241147
lars-av <1086166@ucn.dk>
2022-06-02 00:04:24 +0200

    Set up most of the backedn with posts

--- resources/views/partials/specific-house-post/house-intro.blade.php
+++ resources/views/partials/specific-house-post/house-intro.blade.php
@@ -1,19 +1,20 @@
+
+
 <section>
     <div class="image-full-width">
-        <h2>Purkærhuset</h2>
+        <h2>{{$fc['house_name']}}</h2>
         <div class="two-sections-wrap">
             <div class="address-section">
-                <p><b>Address:</b> Purkærvej 55 9490 Pandrup</p>
-                <h6><i class="fa-solid fa-house-chimney-medical"></i>1 spot available</h6>
-                <h6><i class="fa-solid fa-user-large"></i>Sleeping night shift</h6>
-                <h6><i class="fa-solid fa-clock"></i>24 hours care</h6>
+                <p><b>Address:</b> {{$fc['address']}}</p>
+
+                @foreach ($fc['house_features'] as $hf)
+                <h6><i class="fa-solid fa-house-chimney-medical"></i>{{$hf['house_features_descriptions']}}</h6>
+                @endforeach
             </div>
             <div class="spots-section">
                 <ul>
                     <p><b>Spots</b></p>
-                    <li><b>§107</b> Temporray housing offer</li>
-                    <li><b>§108</b> Flexible place</li>
-                    <li><b>§107</b> Temporary housing offer</li>
+                    {!! $fc['types_of_spots'] !!}
                 </ul>
             </div>
         </div>
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,16 +1,59 @@
 @extends('layouts.app')
-
 @section('content')
 
-@php $flexibleContent = get_field('this_is_the_first_label', get_the_ID()) @endphp
-@dump($flexibleContent)
+@php $theID = get_the_ID() @endphp
+@php $flexibleContent = get_field('flexiblecontent', get_the_ID()) @endphp
+@php $postType = get_post_type($theID) @endphp
 
-@php
-    $theID = get_the_ID()
-@endphp
-@dump(get_post_type($theID))
+@if ($postType == 'house')
+
+<div class="wrapper-large">
+
+@if ("true" == "true")
+  <div class="wrap-side-nav">
+    <div class="side-nav">
+      <ul>
+         <a href="#about"><li class="anchor" >About</li></a>
+         <a href="#day-to-day"><li class="anchor" >Day to Day Life</li></a>
+         <a href="#process-of-applying"><li class="anchor" >Process of applying</li></a>
+         <a href="#self-determination"><li class="anchor" >Self Determiation</li></a>
+         <a href="#duty-of-care"><li class="anchor" >Duty of care</li></a>
+         <a href="#involvement-in-development"><li class="anchor" >Involvement In Development</li></a>
+      </ul>
+    </div>
+  </div>
+@endif
+
+<div class="wrap-side-content">
+@dump($flexibleContent)
+{{-- 
+  id="{{str_replace(' ', '', $fc['section_name'])}}" --}}
 
 @foreach ($flexibleContent as $fc)
+@if ($fc['acf_fc_layout'] == 'introduction_section')
+  @dump($fc['house_features'])
+  @include('partials/specific-house-post/house-intro')
+@endif
+@if ($fc['acf_fc_layout'] == 'section:_text_image_text')
+  @include('partials/specific-house-post/about-house')
+@endif
+@if ($fc['acf_fc_layout'] == 'section:_4_by_4')
+  @include('partials/specific-house-post/activities')
+@endif
+@if ($fc['acf_fc_layout'] == 'section:_interactive_cards')
+  @include('partials/specific-house-post/responsibility-section')
+@endif
+@if ($fc['acf_fc_layout'] == 'section:_4_images_with_text_between')
+  @include('partials/specific-house-post/living-conditions')
+@endif
+</div>
+</div>
+
+@endforeach
+
+@endif
+
+{{-- @foreach ($flexibleContent as $fc)
 @if ($fc['acf_fc_layout'] == 'insideflexcontent1')
 <h1>COOL</h1>
 <p>{{ $fc['firstactualacfinsidecontainer'] }}</p>
@@ -34,38 +77,5 @@
 @php
   $post_fields = get_fields(29);
 @endphp
-@dump($post_fields)
-
-<div class="wrapper-large">
-  @if ("true" == "true")
-  <div class="wrap-side-nav">
-    <div class="side-nav">
-      <ul>
-         <a href="#about"><li class="anchor" >About</li></a>
-         <a href="#day-to-day"><li class="anchor" >Day to Day Life</li></a>
-         <a href="#process-of-applying"><li class="anchor" >Process of applying</li></a>
-         <a href="#self-determination"><li class="anchor" >Self Determiation</li></a>
-         <a href="#duty-of-care"><li class="anchor" >Duty of care</li></a>
-         <a href="#involvement-in-development"><li class="anchor" >Involvement In Development</li></a>
-      </ul>
-    </div>
-  </div>
-  @endif
-
-<div class="wrap-side-content">
-@include('partials/specific-house-post.house-intro')
-@include('partials/specific-house-post.about-house')
-@include('partials/specific-house-post.activities')
-@include('partials/specific-house-post.responsibility-section')
-@include('partials/specific-house-post.living-conditions')
-</div>
-</div>
-{{--   @while(have_posts()) @php the_post() @endphp
-
-    @include('partials.content-single-'.get_post_type())
-
-    I am a post - Check single.blade for my markup
-    whetever
-    Forture lars - make IF statement that checks the post's type and changes markup depending wether it is house or news
-  @endwhile --}}
+@dump($post_fields) --}}
 @endsection
